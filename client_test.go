9c22acb3b8f0e1f2dca4a81b0c6307a84ec571ff
hadrianl <137150224@qq.com>
2020-06-11 18:32:47 +0800

    add more test

--- client_test.go
+++ client_test.go
@@ -21,52 +21,101 @@ func TestClient(t *testing.T) {
 	log, _ = zap.NewDevelopment() // log is default for production(json encode, info level), set to development(console encode, debug level) here
 	defer log.Sync()
 	runtime.GOMAXPROCS(4)
-	var err error
-	ibwrapper := new(Wrapper)
-	ic := NewIbClient(ibwrapper)
-	err = ic.Connect("localhost", 7497, 0)
-	if err != nil {
+
+	ic := NewIbClient(new(Wrapper))
+
+	if err := ic.Connect("localhost", 7497, 0); err != nil {
 		log.Panic("failed to connect", zap.Error(err))
-		return
 	}
 
 	ic.SetConnectionOptions("+PACEAPI")
-	err = ic.HandShake()
-	if err != nil {
+
+	if err := ic.HandShake(); err != nil {
 		log.Panic("failed to hand shake", zap.Error(err))
-		return
 	}
 	ic.Run()
 
-	ic.ReqCurrentTime()
-	ic.ReqAutoOpenOrders(true)
-	ic.ReqAccountUpdates(true, "")
-	ic.ReqExecutions(ic.GetReqID(), ExecutionFilter{})
-
-	hsi := Contract{ContractID: 415314929, Symbol: "HSI", SecurityType: "FUT", Exchange: "HKFE"}
-	ic.ReqHistoricalData(ic.GetReqID(), &hsi, "", "4800 S", "1 min", "TRADES", false, 1, true, nil)
-	ic.ReqMktDepth(ic.GetReqID(), &hsi, 5, true, nil)
-	ic.ReqContractDetails(ic.GetReqID(), &hsi)
+	// ####################### request base info ##################################################################
+	// ic.ReqCurrentTime()
+	// ic.ReqAutoOpenOrders(true)
+	// //ic.ReqAutoOpenOrders(false)
+	// ic.ReqAccountUpdates(true, "")
+	// //ic.ReqAccountUpdates(false, "")
+	// ic.ReqExecutions(ic.GetReqID(), ExecutionFilter{})
 	// ic.ReqAllOpenOrders()
-	// ic.ReqMktData(ic.GetReqID(), &hsi, "", false, false, nil)
-	ic.ReqPositions()
-	// ic.ReqRealTimeBars(ic.GetReqID(), &hsi1909, 5, "TRADES", false, nil)
+	// ic.ReqPositions()
+	// // ic.CancelPositions()
+	// ic.ReqAccountUpdatesMulti(ic.GetReqID(), "DU1382837", "", true)
+	// // ic.CancelAccountUpdatesMulti()
+	// ic.ReqPositionsMulti(ic.GetReqID(), "DU1382837", "")
+	// // ic.CancelPositionsMulti()
+	// tags := []string{"AccountType", "NetLiquidation", "TotalCashValue", "SettledCash",
+	// 	"AccruedCash", "BuyingPower", "EquityWithLoanValue",
+	// 	"PreviousEquityWithLoanValue", "GrossPositionValue", "ReqTEquity",
+	// 	"ReqTMargin", "SMA", "InitMarginReq", "MaintMarginReq", "AvailableFunds",
+	// 	"ExcessLiquidity", "Cushion", "FullInitMarginReq", "FullMaintMarginReq",
+	// 	"FullAvailableFunds", "FullExcessLiquidity", "LookAheadNextChange",
+	// 	"LookAheadInitMarginReq", "LookAheadMaintMarginReq",
+	// 	"LookAheadAvailableFunds", "LookAheadExcessLiquidity",
+	// 	"HighestSeverity", "DayTradesRemaining", "Leverage", "$LEDGER:ALL"}
+	// ic.ReqAccountSummary(ic.GetReqID(), "All", strings.Join(tags, ","))
+	// ic.CancelAccountSummary()
 
-	tags := []string{"AccountType", "NetLiquidation", "TotalCashValue", "SettledCash",
-		"AccruedCash", "BuyingPower", "EquityWithLoanValue",
-		"PreviousEquityWithLoanValue", "GrossPositionValue", "ReqTEquity",
-		"ReqTMargin", "SMA", "InitMarginReq", "MaintMarginReq", "AvailableFunds",
-		"ExcessLiquidity", "Cushion", "FullInitMarginReq", "FullMaintMarginReq",
-		"FullAvailableFunds", "FullExcessLiquidity", "LookAheadNextChange",
-		"LookAheadInitMarginReq", "LookAheadMaintMarginReq",
-		"LookAheadAvailableFunds", "LookAheadExcessLiquidity",
-		"HighestSeverity", "DayTradesRemaining", "Leverage", "$LEDGER:ALL"}
-	ic.ReqAccountSummary(ic.GetReqID(), "All", strings.Join(tags, ","))
-	// ic.ReqFamilyCodes()
-	// ic.ReqMatchingSymbols(ic.GetReqID(), "HSI")
+	// ########################## request market data #################################################
+	// hsi := Contract{ContractID: 389657869, Symbol: "HSI", SecurityType: "FUT", Exchange: "HKFE"}
+	// ic.ReqHistoricalData(ic.GetReqID(), &hsi, "", "4800 S", "1 min", "TRADES", false, 1, true, nil)
+	// // ic.CancelHistoricalData()
+	// ic.ReqMktDepth(ic.GetReqID(), &hsi, 5, true, nil)
+	// //ic.CancelMktDepth()
+	// ic.ReqMktData(ic.GetReqID(), &hsi, "", false, false, nil)
+	// // ic.CancelMktData()
+	// ic.ReqRealTimeBars(ic.GetReqID(), &hsi, 5, "TRADES", false, nil)
+	// // ic.CancelRealTimeBars()
+	// ic.ReqTickByTickData(ic.GetReqID(), &hsi, "Last", 5, false)
+	// // ic.CancelTickByTickData()
+	// ic.ReqHistoricalTicks(ic.GetReqID(), &hsi, "20190916 09:15:00", "", 100, "Trades", false, false, nil)
+	// ic.ReqHistogramData(ic.GetReqID(), &hsi, false, "3 days")
+	// // ic.CancelHistogramData()
+
+	// ############################# request combo historical data ################################################
+	// hsiSpread := new(Contract)
+	// hsiSpread.Symbol = "HSI"
+	// hsiSpread.SecurityType = "BAG"
+	// hsiSpread.Currency = "HKD"
+	// hsiSpread.Exchange = "HKFE"
+	// leg1 := ComboLeg{ContractID: 389657869, Ratio: 1, Action: "BUY", Exchange: "HKFE"}
+	// leg2 := ComboLeg{ContractID: 424418656, Ratio: 1, Action: "SELL", Exchange: "HKFE"}
+	// hsiSpread.ComboLegs = append(hsiSpread.ComboLegs, leg1, leg2)
+	// ic.ReqHistoricalData(ic.GetReqID(), hsiSpread, "", "4800 S", "1 min", "TRADES", false, 1, false, nil)
+
+	// ######################### request contract ############################################################
+	// hsi := Contract{Symbol: "HSI", SecurityType: "FUT", Exchange: "HKFE"}
+	// ic.ReqContractDetails(ic.GetReqID(), &hsi)
+	// ic.ReqMatchingSymbols(ic.GetReqID(), "IB")
+
+	// ######################### market scanner #############################################################
 	// ic.ReqScannerParameters()
-	// ic.ReqTickByTickData(ic.GetReqID(), &hsi2003, "Last", 5, false)
-	// ic.ReqHistoricalTicks(ic.GetReqID(), &hsi1909, "20190916 09:15:00", "", 100, "Trades", false, false, nil)
+	// scanSub := new(ScannerSubscription)
+	// scanSub.Instrument = "FUT.HK"
+	// scanSub.LocationCode = "FUT.HK"
+	// scanSub.ScanCode = "HOT_BY_VOLUME"
+	// t1 := TagValue{"usdMarketCapAbove", "10000"}
+	// t2 := TagValue{"optVolumeAbove", "1000"}
+	// t3 := TagValue{"avgVolumeAbove", "100000000"}
+	// ic.ReqScannerSubscription(ic.GetReqID(), scanSub, nil, nil)
+	// ic.CancelScannerSubscription()
+
+	// ############################### display group ########################################################
+	// ic.QueryDisplayGroups(ic.GetReqID())
+	// subGroupID := ic.GetReqID()
+	// ic.SubscribeToGroupEvents(subGroupID, 4)
+	// ic.UpdateDisplayGroup(subGroupID, "389657869@HKFE")
+	// // ic.UnsubscribeFromGroupEvents(subGroupID)
+
+	// ############################ others #########################################################################
+
+	ic.ReqFamilyCodes()
+	// ic.ReqScannerParameters()
 	// ic.ReqManagedAccts()
 	// ic.ReqSoftDollarTiers(ic.GetReqID())
 	// ic.ReqNewsProviders()
@@ -77,16 +126,15 @@ func TestClient(t *testing.T) {
 	// ic.ReqMktDepthExchanges()
 	// ic.ReqMatchingSymbols(ic.GetReqID(), "HSI")
 	// ic.ReqSecDefOptParams(ic.GetReqID(), "HSI", "", "IND", 1328298)
-	// ic.ReqHistogramData(ic.GetReqID(), &hsi1909, false, "3 days")
 	// ic.ReqGlobalCancel()
 	// ic.ReqIDs()
-	// ic.ReqAccountUpdatesMulti(ic.GetReqID(), "DU1382837", "", true)
-	// ic.ReqPositionsMulti(ic.GetReqID(), "DU1382837", "")
 	// lmtOrder := NewLimitOrder("BUY", 26640, 1)
 	// mktOrder := NewMarketOrder("BUY", 1)
 	// ic.PlaceOrder(ibwrapper.GetNextOrderID(), &hsi1909, lmtOrder)
 	// ic.CancelOrder(ibwrapper.OrderID() - 1)
-	ic.ReqHeadTimeStamp(ic.GetReqID(), &hsi, "TRADES", false, 1)
+	// ic.ReqHeadTimeStamp(ic.GetReqID(), &hsi, "TRADES", false, 1)
+	// ic.ReqNewsBulletins(true)
+	// ic.ReqFundamentalData()
 
 loop:
 	for {
